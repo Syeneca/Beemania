f3f66b620bb22205167a61c2a64fac802717dff9
Alessandro Monti <montialessandro.a.n@gmail.com>
2024-10-24 00:53:52 -0300

    Initial commit

---
+++ src/client/java/beemania/syeneca_mod/BeemaniaClient.java
@@ -0,0 +1,10 @@
+package beemania.syeneca_mod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BeemaniaClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
